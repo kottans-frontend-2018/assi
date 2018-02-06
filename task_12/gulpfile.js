5276654e266301ce049a09d444d66d53d9d47a31
Tania Maslivets <t.mykhailiuk1@gmail.com>
2018-02-06 11:02:35 +0200

    fixed config file

--- task_12/gulpfile.js
+++ task_12/gulpfile.js
@@ -1,15 +1,15 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var cleanCSS = require('gulp-clean-css');
-var imagemin = require('gulp-imagemin');
-var concat = require('gulp-concat');
-var uglify = require('gulp-uglify');
-const autoprefixer = require('gulp-autoprefixer');
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    cleanCSS = require('gulp-clean-css'),
+    imagemin = require('gulp-imagemin'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
+    autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('sass', function () {
     return gulp.src('src/css/*.scss')
         .pipe(sass())
-        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(cleanCSS())
         .pipe(autoprefixer({
             browsers: ['last 2 versions'],
             cascade: false
@@ -23,24 +23,11 @@ gulp.task('img', function () {
         .pipe(gulp.dest('dist/img'));
 });
 
-
-gulp.task('concat', function () {
-    return gulp.src([
-        'src/js/loader.js',
-    ])
-        .pipe(concat('main.js'))
-        .pipe(uglify())
-        .pipe(gulp.dest('dist/js'));
-});
-
-
-
 gulp.task('watch', function () {
     gulp.watch("src/css/**/*.scss", ['sass']);
     gulp.watch("src/img/**/*", ['img']);
-    gulp.watch("src/js/*", ['js']);
 });
 
 
-gulp.task("default", ['img', 'sass', 'concat', 'watch']);
+gulp.task("default", ['img', 'sass', 'watch']);
 
